fec9bbd5d7490a5e953b63ff35767eb0c3c4e301
030238220060-hub <030238220060@st.buh.edu.vn>
2026-03-18 00:24:18 +0700

    them ten routes/web.php NguyenTanHien

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,6 @@
 <?php
+use Illuminate\Support\Facades\Route; 
 
-Route::get('/baotran123', function () {
-    return "Nguyen Ngoc Bao Tran";
-});
-Route::get('/kapee2812', function () {
-    return "Dang Thi Kim Phuong";
-});
-
-Route::get('/buithanhhuy', function () {
-    return "Bui Thanh Huy";
-});
-Route::get('/doanquochuy', function () {
-    return 'Đoan Quoc Huy';
+Route::get('/tanhien', function () {
+    return 'Nguyễn Tấn Hiển';
 });
