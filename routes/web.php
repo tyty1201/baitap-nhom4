6f7dfa9dd5d941c666975a7847d3d48cb6d7ec4d
thiistired <qthiiilt@gmail.com>
2026-03-18 14:23:34 +0700

    quynhthi hoan thanh yeu cau 7.3

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,11 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MovieController;
+
+
 Route::get('/quynhthi', function () {
     return 'Dao Quynh Thi';
 });
+
+Route::get('/top-budget-movies', [MovieController::class, 'getTopBudget']);
